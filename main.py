4d606477ad3a1bbda479f4b777fa0184784782e0
tmathiesen <tmathiesen@coherebeauty.com>
2024-07-18 20:29:04 -0500

    update

--- main.py
+++ main.py
@@ -2,54 +2,67 @@ import random
 class Character:
     def __init__(self, name, health, energy, experience, level):
         self.name=name
+        self.health=health
+        self.energy=energy
+        self.experience=experience
+        self.level=level
 
-class Python_hero(Character):
-    experience=0
-    level=1
-    health=100
-    energy=100
     def hero_attack(self):
         self.energy-=5
         self.experience+=1
 
-class Java_monster(Character):
-    experience=10
-    level=3
-    health=100
-    energy=100
     def monster_attack(self):
         self.energy-=10
 
+    def __repr__(self):
+      return f'\nCharacter: {self.name}\nHealth: {self.health}\nEnergy: {self.energy}\nExperience: {self.experience}\nLevel: {self.level}'
+
+hero=Character("Python hero",100,100,0,1)
+monster=Character("Java monster",100,100,10,3)
+
 def gameplay(hero,monster):
-    return Python_hero(hero)
-    return Java_monster(monster)
-    while Python_hero.health>0:
-      option=input("Type 1 for attack, type 2 for run")
-      if option not in [1,2]:
-        raise ValueError("1 for attack, 2 for run only")
-      else:
-        if option==1:
-          block1=randint(1,10)
-          if block1 in [3,7]:
-            monster.energy-=5
-          else:
-            hero_attack(hero)
-            monster.health-=10
-          block2=randint(1,10)
-          if block2 in [2,8]:
-            hero.energy-=2
-          else:
-            monster_attack(monster)
-            hero.health-=15
-          if hero.experience>=5:
-            hero.level+=1
-            hero.experience-=5
-          if hero.health<=20:
-            return "Warning!!! Your health is low!"
-          elif hero.health<=0:
-            return "You died."
-            break
-          if monster.health<=0:
-            return "You win!"
+    print(hero)
+    print(monster)
+    while hero.health>0:
+        option=input("\nType 1 for attack, type 2 for run ")
+        if int(option) not in [1,2]:
+            raise ValueError("1 for attack, 2 for run only")
         else:
-          return 'Game over'
+            if int(option)==1:
+                print('The hero is attacking the monster')
+                block1=random.randint(1,10)
+                if int(block1) in [3,7]:
+                    monster.energy-=5
+                    print("The monster blocked the hero's attack")
+                else:
+                    Character.hero_attack(hero)
+                    monster.health-=10
+                    print("The hero successfully landed a hit!")
+                block2=random.randint(1,10)
+                if int(block2) in [2,8]:
+                    hero.energy-=2
+                    print("The hero blocked the monster's attack")
+                else:
+                    Character.monster_attack(monster)
+                    hero.health-=15
+                    print("The monster attacked the hero")
+                if hero.experience>=5:
+                    hero.level+=1
+                    hero.experience-=5
+                    print('Hero leveled up!')
+                if hero.health<=20:
+                    return "Warning!!! Your health is low!"
+                elif hero.health<=0:
+                    return "You died."
+                    break
+                if monster.health<=0:
+                    return "You win!"
+            else:
+                print('Game over')
+                break
+            print(hero)
+            print(monster)
+        
+if __name__ == "__main__":
+  gameplay(hero,monster)
+  pass
